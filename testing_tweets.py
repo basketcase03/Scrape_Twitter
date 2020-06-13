5e05d479d830cff0ea70dbfd122bf3d2f6e631b1
Muskan Khajuria <47385642+basketcase03@users.noreply.github.com>
2020-06-13 17:05:29 +0530

    Update testing_tweets.py

--- testing_tweets.py
+++ testing_tweets.py
@@ -6,9 +6,9 @@ db = client['scrapped_tweets_fin']
 tweets = db["tweets"]
 
 file=open("tweets.txt","a",encoding="utf-8")
-count=0
+
 for tweet in tweets.find():
     print(tweet["text"])
-    file.write("\n"+str(count)+tweet["text"])
-    count+=1
+    file.write("\n"+tweet["text"])
+
 file.close()
